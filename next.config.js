60eeb1c5e223b28dd0b2fb81c4eb7c7be95a51be
yuki110580 <yuki110580@gmail.com>
2025-06-10 21:36:09 +0900

    Fix:yml15

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  srcDir: 'src', // 👈 これを追加することで src/app が認識される
   output: 'standalone',
+  env: {
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT || 'https://api.pos-app.example.com',
+  },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
